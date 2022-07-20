fca4d314909947e1365f4d55b8e6b6b8f3e1e0e4
hyperspacetech <phil.kotze@aurizon.com.au>
2022-07-20 21:35:55 +1000

    Add include for time.h

--- src/libsstp-log/sstp-log-private.h
+++ src/libsstp-log/sstp-log-private.h
@@ -21,6 +21,8 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
+#include "time.h"
 #ifndef __SSTP_LOG_PRIVATE_H__
 #define __SSTP_LOG_PRIVATE_H__
 
